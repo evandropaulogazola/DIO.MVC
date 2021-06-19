96bd6a14a8f56261d0b851c7e79f08e7a4049fda
Evandro Paulo Gazola <evandropaulogazola@hotmail.com>
2021-06-19 09:46:57 -0300

    Primeiro Teste via VS

---
+++ DIO.CursoMVC/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace DIO.CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated security=True");
+        }
+    }
+}
